3bf20c86c4465b9b401caefaab84eaf94e97c893
rikardokz <rikardo_kz@live.com.pt>
2020-09-15 16:25:15 +0100

    else and each iterations

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+const button = document.getElementById("not-motivated");
+
 // element and HTML content in script tag
 const source = document.getElementById("entry-template").innerHTML;
 // compiled by handlebards
@@ -6,8 +8,11 @@ const template = Handlebars.compile(source);
 const context = {
   name: "Ricardo",
   age: 34,
+  motivated: true,
+  month: [1, 2, 3, 4, 5, 6, 7, 8, 9],
 };
 
+console.log(context.motivated);
 // added the context to a template
 const compiledHtml = template(context);
 // selected a html element in the "real" html
